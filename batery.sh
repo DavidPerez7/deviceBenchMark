a40a2575ffd0690445b0c946f187c7bfd5f92984
DavidPerez7 <juan_dperez51@soy.sena.edu.co>
2025-10-29 13:30:45 -0500

    promedio de consumo mAh

---
+++ batery.sh
@@ -0,0 +1,21 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+declare numValores=0
+echo "Digite el numero de valores a promediar: "
+read numValores
+
+declare -a consumos
+for ((i=1; i<=numValores; i++)); do
+    consumoMAH=$(cat /sys/class/power_supply/battery/current_now)
+    echo "consumo en mAh: $consumoMAH"
+    consumos+=("$consumoMAH")
+done
+
+declare sumas=0
+for ((j=0; j<${#consumos[@]}; j++)); do
+    sumas=$(($sumas + ${consumos[j]}))
+done
+
+promedioConsumo=$(($sumas / ${#consumos[@]}))
+
+echo "Consumo promedio en mAh: $promedioConsumo"
