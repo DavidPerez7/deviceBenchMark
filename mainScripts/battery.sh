d193350c43b8d865944f96fd50a6406723f8a2bf
DavidPerez7 <juan_dperez51@soy.sena.edu.co>
2025-11-13 22:42:51 -0500

    correciones battery, organizacion, porcentaje de consumo

---
+++ mainScripts/battery.sh
@@ -0,0 +1,29 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+declare numConsultas=100
+declare capacidadMAH=$(su -c "cat /sys/class/power_supply/battery/charge_full")
+echo "-- CONSULTAS DE CONSUMO DE BATERÍA --"
+sleep 1
+echo "Capacidad de la batería en mAh: $capacidadMAH"
+echo "Número de consultas a realizar: $numConsultas"
+
+declare -a consumos
+for ((i=1; i<=numConsultas; i++)); do
+    consumoMAH=$(su -c "cat /sys/class/power_supply/battery/current_now")
+    echo "consumo en mAh: $consumoMAH"
+    consumos+=("$consumoMAH")
+    sleep 1
+done
+
+declare sumas=0
+for ((j=0; j<${#consumos[@]}; j++)); do
+    sumas=$(($sumas + ${consumos[j]}))
+done
+
+promedioConsumo=$(($sumas / ${#consumos[@]}))
+declare promedioPorcentaje=$((($promedioConsumo / $capacidadMAH) * 100))
+
+sleep 1
+echo "Consumo promedio en mAh: $promedioConsumo"
+echo "Consumo promedio en porcentaje: $promedioPorcentaje%"
+echo "-- FIN DE CONSULTAS --"
