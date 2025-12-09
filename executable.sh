9e88066524ae105b4009c8ec5c0bcac35bc7d6b8
DavidPerez7 <juan_dperez51@soy.sena.edu.co>
2025-12-09 18:34:48 -0500

    correcion de executable

--- executable.sh
+++ executable.sh
@@ -2,8 +2,9 @@
 
 # Otorga permisos de ejecución a los scripts principales
 echo '= OTORGANDO PERMISOS DE EJECUCION ='
-chmod +x "$(dirname "$0")/scripts/battery.sh"
-chmod +x "$(dirname "$0")/scripts/setopt.sh"
+DIR=$(pwd)
+chmod +x "$DIR/scripts/battery.sh"
+chmod +x "$DIR/scripts/setopt.sh"
 echo "Permisos de ejecución otorgados (battery.sh, setopt.sh)."
 
 declare opcion
@@ -14,12 +15,12 @@ read opcion
 
 if [ "$opcion" -eq 1 ]; then
     # Ejecuta el script de análisis de batería
-    su -c "bash \"$(dirname "$0")/scripts/battery.sh\""
+    su -c "bash \"$DIR/scripts/battery.sh\""
     exit 0
 fi
 
 if [ "$opcion" -eq 2 ]; then
     # Ejecuta el script de configuración de optimizaciones
-    su -c "bash \"$(dirname "$0")/scripts/setopt.sh\""
+    su -c "bash \"$DIR/scripts/setopt.sh\""
     exit 0
 fi
